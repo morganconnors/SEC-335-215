fecc235bacca8d42f92f44b8e41145bfcbb875b5
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-09-23 11:17:32 -0400

    Update script.sh

--- activity-3.1/script.sh
+++ activity-3.1/script.sh
@@ -3,11 +3,16 @@
 prefix = $1
 port = $2
 
-echo "host,port"
+echo "inputs"
+echo "prefix - $prefix"
+echo "port - $port"
+
+echo "host,port,state,service"
 
 for i in {1..254}; do
     ip="10.0.5.$i"
 
+    echo "$ip"
     nmap -p $port $ip | grep 53
     
 done
